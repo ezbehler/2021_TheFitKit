53e263ac39155dca9c6b124eaefcb1112dc6d8af
ttomczak3 <ttomczak3@student.cvtc.edu>
2021-04-20 01:09:32 -0700

    Added login and registration.

---
+++ FitKit/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using FitKit.Areas.Identity.Data;
+using FitKit.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(FitKit.Areas.Identity.IdentityHostingStartup))]
+namespace FitKit.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<FitKitDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("FitKitDbContextConnection")));
+
+                services.AddDefaultIdentity<FitKitUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<FitKitDbContext>();
+            });
+        }
+    }
+}
--- FitKit/Controllers/HomeController.cs
+++ FitKit/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using FitKit.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
