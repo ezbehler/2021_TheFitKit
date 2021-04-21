4b63eb3313dfc843e4d3d9948e1ab068cebda991
seaem1998 <chantellemarek@yahoo.com>
2021-04-21 11:18:12 -0500

    Added the Diet and Exercise pages. -Chantelle

--- FitKit/Controllers/HomeController.cs
+++ FitKit/Controllers/HomeController.cs
@@ -29,7 +29,16 @@ namespace FitKit.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Diet(){
+            return View();
+        }
+
+        public IActionResult Exercise()
+        {
+            return View();
+        }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
