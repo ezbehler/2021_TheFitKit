dcb5cb546e63c0315e9f557bd51eee0820038edd
seaem1998 <chantellemarek@yahoo.com>
2021-05-03 15:20:30 -0500

    JavaScript file is linked to Diet and Tracker page. Can not get the CSS file to work on the pages.

--- FitKit/wwwroot/js/site.js
+++ FitKit/wwwroot/js/site.js
@@ -2,3 +2,29 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+// For the Tracker Page
+function calories(ateCals, burnedCals) {
+    var leftOverCals = parseInt(ateCals) - parseInt(burnedCals);
+    alert(leftOverCals + " calorie(s) have not been used.");
+}
+
+// Accordion Code for Diet and Exercise Page
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
+
+        /* Toggle between hiding and showing the active panel */
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
